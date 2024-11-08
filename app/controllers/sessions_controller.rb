02dcc86ca425a9943677f3f1beeb647a6aa29711
kekaze <jroyceyanson@gmail.com>
2024-11-08 17:32:54 +0800

    :card_file_box: implemented shortcutuser table purging on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+
+  @@roles = {
+    1 => "Requestor",
+    2 => "Reviewer",
+    3 => "Approver",
+    4 => "Specialist",
+    5 => "Custodian",
+    6 => "Finance",
+    9 => "Administrator"
+  }
+
   def index # shows login page for shortcut process (no credentials)
     render layout: 'application'
   end
@@ -9,18 +20,12 @@ class SessionsController < ApplicationController
       @username = params[:username]
       @role_id = params[:role].to_i
 
-      @roles = {
-        1 => "Requestor",
-        2 => "Reviewer",
-        3 => "Approver",
-        4 => "Specialist",
-        5 => "Custodian",
-        6 => "Finance",
-        9 => "Administrator"
-      }
-      session[:role] = @role_id
-
-      if @username && @role_id
+      @shortcut_user = Shortcutuser.new(username: @username, role_id: @role_id)
+      
+      if @shortcut_user.save
+        @roles = @@roles
+        session[:user_id] = @shortcut_user.id
+        session[:role] = @role_id
         session[:isShortcut] = true
         redirect_to worsystem_path unless @role_id > 4
         redirect_to warehouse_path if @role_id == 5
@@ -34,16 +39,7 @@ class SessionsController < ApplicationController
 
       redirect_to login_path and return if !@user
 
-      @roles = {
-        1 => "Requestor",
-        2 => "Reviewer",
-        3 => "Approver",
-        4 => "Specialist",
-        5 => "Custodian",
-        6 => "Finance",
-        9 => "Administrator"
-      }
-
+      @roles = @@roles
       @role_id = @user.role_id
 
       session[:role], session[:user_id], session[:profile], session[:isShortcut] = @role_id, @user.id, "#{@user.first_name} #{@user.last_name} (#{@roles[@role_id]})", false
@@ -56,7 +52,20 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    if session[:isShortcut] 
+    @shortcut_user = Shortcutuser.find(session[:user_id])
+    @max_id = ActiveRecord::Base.connection.select_value("SELECT MAX('id') FROM shortcutusers")
+
+    if @shortcut_user
+      @shortcut_user.destroy
+    end
+
+    if @max_id
+      ActiveRecord::Base.connection.reset_pk_sequence!('shortcutusers', @max_id)
+    else
+      ActiveRecord::Base.connection.reset_pk_sequence!('shortcutusers')
+    end
+
+    if session[:isShortcut]
       session.clear
       redirect_to root_path
       return
